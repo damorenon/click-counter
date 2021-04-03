5ddd0d9d2c9a668181511a63df5a8eb581f17823
damorenon <damorenon@gmail.com>
2021-04-02 19:03:56 -0500

    Refactoring tests

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import "./App.css";
 function App() {
   return (
     <div data-test="component-app">
-      <h1>App</h1>
+      <h1 data-test="counter-display">The counter currently is</h1>
+      <button data-test="increment-button">Increment counter</button>
     </div>
   );
 }
